b100fa30c74241977e12857f58c385cb6ced6183
damienbod <damien_bod@hotmail.com>
2018-06-03 20:13:59 +0200

    Adding some input controls

--- AspNetCoreMvcDynamicViews/Views/Shared/Components/DynamicDisplay/DynamicDisplayModel.cs
+++ AspNetCoreMvcDynamicViews/Views/Shared/Components/DynamicDisplay/DynamicDisplayModel.cs
@@ -3,5 +3,11 @@
     public class DynamicDisplayModel
     {
         public int NoOfHoles { get; set; }
+
+        public int Height { get; set; }
+
+        public int NoOfBoxes { get; set; }
+
+        public int BoxWidth { get; set; }
     }
 }
