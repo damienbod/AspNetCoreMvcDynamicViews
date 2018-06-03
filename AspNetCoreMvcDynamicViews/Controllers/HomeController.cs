a5e0d39f962a6b560c44d8a694c196f6c5a4e4e2
damienbod <damien_bod@hotmail.com>
2018-06-03 19:42:16 +0200

    adding an MVC Core app

---
+++ AspNetCoreMvcDynamicViews/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreMvcDynamicViews.Models;
+using AspNetCoreMvcDynamicViews.Views.Shared.Components.DynamicDisplay;
+
+namespace AspNetCoreMvcDynamicViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new MyDisplayModel();
+            model.DynamicDisplayData = new DynamicDisplayModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(MyDisplayModel myDisplayModel)
+        {
+            // save data to db...
+            return View("Index", myDisplayModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
